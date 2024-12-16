5cac4660c7673564723937d1dcc6060b54b9c5ec
AartiGhorpade <89845285+AartiGhorpade@users.noreply.github.com>
2024-12-16 22:57:01 +0530

    more data

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const fs = require('fs')
+const path = require('path')
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.set('view engine', 'ejs')
+app.use(express.static(path.join(__dirname, "public")))
+
+app.get('/', (req, resp) => {
+    fs.readdir("./files", (err, files) => {
+        resp.render("index", { files: files })
+    })
+})
+
+app.get('/file/:filename', (req, resp) => {
+    fs.readFile(`./files/${req.params.filename}`, "utf-8", (err, filedata) => {
+        resp.render("show", { filename: req.params.filename, filedata : filedata })
+    })
+})
+app.post('/create', (req, resp) => {
+    const fileName = `./files/${req.body.title.split(' ').join('')}.txt`
+    fs.writeFile(fileName, req.body.details, () => {
+        resp.redirect('/')
+    })
+})
+
+app.listen(3002, () => {
+    console.log("app is listening on 3002 port")
+})
